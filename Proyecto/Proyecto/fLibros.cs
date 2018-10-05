4cc0e347d486d0551751db34625b7fdb5173871e
Andres Julian Garcia <andresjgarciaperez@outlook.com>
2018-10-04 21:43:10 -0500

    RAMS

--- Proyecto/Proyecto/fLibros.cs
+++ Proyecto/Proyecto/fLibros.cs
@@ -28,5 +28,19 @@ namespace Proyecto
         {
 
         }
+
+        private void fLibros_Load(object sender, EventArgs e)
+        {
+            axAcroPDF1.src = "C:/Users/omar/Downloads/HappyBoiiii-master/Proyecto/Proyecto/Cuentos/elreyleon.pdf";
+            axAcroPDF2.src = "C:/Users/omar/Downloads/HappyBoiiii-master/Proyecto/Proyecto/Cuentos/elzorroyeltigre.pdf";
+            axAcroPDF3.src = "C:/Users/omar/Downloads/HappyBoiiii-master/Proyecto/Proyecto/Cuentos/lasirenita.pdf";
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            axAcroPDF4.src = openFileDialog1.FileName;
+        }
     }
 }
